bd68d964a417901f4a993d03ccfb5ed8152490fe
huydangapi2707 <dangdinhhuyisme@gmail.com>
2024-05-03 22:44:37 +0700

    add api company list

--- BusStationBackend/src/main/java/com/busstation/controllers/CustomResponseEntityExceptionHandler.java
+++ BusStationBackend/src/main/java/com/busstation/controllers/CustomResponseEntityExceptionHandler.java
@@ -4,11 +4,14 @@ import com.busstation.dtos.ApiError;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
@@ -26,9 +29,25 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
             errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
         }
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, errors);
 
         return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<Object> handleAuthentication(AuthenticationException ex, WebRequest request) {
+        ApiError apiError = new ApiError(
+                HttpStatus.BAD_REQUEST, "Wrong username or password");
+        return new ResponseEntity<Object>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Object> handleUsernameOrEmailIsExisted(IllegalArgumentException ex, WebRequest request) {
+        ApiError apiError = new ApiError(
+                HttpStatus.BAD_REQUEST, ex.getMessage());
+        return new ResponseEntity<Object>(
+                apiError, new HttpHeaders(), apiError.getStatus());
     }
 
 }
--- bs-webapp/src/config/apis.js
+++ bs-webapp/src/config/apis.js
@@ -4,7 +4,11 @@ const BASE_URL = 'http://localhost:8080/busstation';
 const endpoints = {
   login: '/api/v1/auth/authenticate',
   register: '/api/v1/auth/register',
+  company_list: '/api/v1/transportation_company/list',
 };
+
+// transportationCompany_list has available request params: kw , page
+// kw stand for company name query
 
 const apis = axios.create({
   baseURL: BASE_URL,
