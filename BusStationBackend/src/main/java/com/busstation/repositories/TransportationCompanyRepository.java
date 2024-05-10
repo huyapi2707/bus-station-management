dc3813db875d3d28739ffba91850cc0f0cb40560
nqhQuocHung <hungmt2426@gmail.com>
2024-05-10 20:49:24 +0700

    Update Verify Bus Company

--- BusStationBackend/src/main/java/com/busstation/repositories/TransportationCompanyRepository.java
+++ BusStationBackend/src/main/java/com/busstation/repositories/TransportationCompanyRepository.java
@@ -8,7 +8,9 @@ import java.util.Map;
 public interface TransportationCompanyRepository {
     List<TransportationCompany> getAll(Map<String,String> params) ;
     TransportationCompany getTransportationCompanyById(int id);
+
     void saveTransportationCompany(TransportationCompany newtransportationCompany);
     void deleteTransportationCompany(int id);
     void updateTransportationCompany(TransportationCompany transportationCompany);
+    void verifiedTransportationCompany(int id);
 }
